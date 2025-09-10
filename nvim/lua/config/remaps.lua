cd2a8a4f93d247fba524176f7ee9c507dcccc40b
Krzysztof Witucki <n3rsti@protonmail.com>
2025-09-10 20:15:34 +0200

    refactor telescope bindings

--- nvim/lua/config/remaps.lua
+++ nvim/lua/config/remaps.lua
@@ -14,16 +14,8 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("i", "<C-h>", "<Left>")
 vim.keymap.set("i", "<C-l>", "<Right>")
 
--- vim.keymap.set("n", "<leader>sa", ":Explore<CR>", { desc = "Open file explorer" })
 vim.keymap.set("n", "<leader>x", ":!go run .<CR>", { desc = "Execute file" })
 
--- vim.keymap.set("i", "<C-j>", 'copilot#Accept("\\<CR>")', {
--- 	expr = true,
--- 	replace_keycodes = false,
--- })
--- vim.g.copilot_no_tab_map = true
-
-vim.keymap.set("n", "<leader>/", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
@@ -36,20 +28,7 @@ local map = function(keys, func, desc, mode)
 	mode = mode or "n"
 	vim.keymap.set(mode, keys, func, { desc = "LSP: " .. desc })
 end
+
 map("rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
 map("ca", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-
-map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
--- Jump to the definition of the word under your cursor.
---  This is where a variable was first declared, or where a function is defined, etc.
---  To jump back, press <C-t>.
-map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
--- Jump to the type of the word under your cursor.
---  Useful when you're not sure what type a variable is and you want to see
---  the definition of its *type*, not where it was *defined*.
-map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,23 +1,45 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 	config = function()
-		require("telescope").setup({
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
+		local themes = require("telescope.themes")
+
+		local function map(keys, func, desc, mode)
+			mode = mode or "n"
+			vim.keymap.set(mode, keys, func, { desc = desc })
+		end
+
+		-- Setup telescope
+		telescope.setup({
 			pickers = {
 				buffers = {
-					sort_lastused = true,
 					sort_mru = true,
+					ignore_current_buffer = true,
 				},
 			},
 		})
 
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>sf", function()
+		telescope.load_extension("fzf")
+
+		-- File and search keymaps
+		map("<leader>sf", function()
 			builtin.find_files({ no_ignore = true })
-		end, { desc = "Telescope find files" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Telescope live grep" })
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Telescope buffers" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-		vim.keymap.set("n", "<C-p>", builtin.lsp_document_symbols, { desc = "Telescope document symbols" })
+		end, "Telescope find files")
+		map("<leader>sg", builtin.live_grep, "Telescope live grep")
+		map("<leader><leader>", builtin.buffers, "Telescope buffers")
+		map("<leader>fh", builtin.help_tags, "Telescope help tags")
+		map("<C-p>", builtin.lsp_document_symbols, "Telescope document symbols")
+		map("<leader>/", builtin.current_buffer_fuzzy_find, "[/] Fuzzily search in current buffer")
+
+		-- LSP keymaps
+		map("gr", builtin.lsp_references, "LSP: [G]oto [R]eferences")
+		map("gi", builtin.lsp_implementations, "LSP: [G]oto [I]mplementation")
+		map("gd", builtin.lsp_definitions, "LSP: [G]oto [D]efinition")
+		map("grt", builtin.lsp_type_definitions, "LSP: [G]oto [T]ype Definition")
 	end,
 }
