82911d5fc06a915603707a774587714db8c2fd04
Krzysztof Witucki <n3rsti@protonmail.com>
2025-09-10 20:14:32 +0200

    simplify waybar

--- waybar/scripts/get_weather.sh
+++ waybar/scripts/get_weather.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 for i in {1..5}
 do
-    text=$(curl -s "https://wttr.in/$1?format=1")
+    text=$(curl -s "https://wttr.in?format=1")
     if [[ $? == 0 ]]
     then
         text=$(echo "$text" | sed -E "s/\s+/ /g")
-        tooltip=$(curl -s "https://wttr.in/$1?format=4")
+        tooltip=$(curl -s "https://wttr.in?format=4")
         if [[ $? == 0 ]]
         then
             tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
@@ -15,4 +15,3 @@ do
     fi
     sleep 2
 done
-echo "{\"text\":\"error\", \"tooltip\":\"error\"}"
