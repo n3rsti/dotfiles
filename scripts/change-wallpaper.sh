c48eac23b30c907cfc4cc7bb7bea3d340d52dca1
Krzysztof Witucki <n3rsti@protonmail.com>
2026-03-05 14:32:26 +0100

    change wallpaper daemon, add wallpapers, fix poe scroll click

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ln -sf $1 ~/.config/dotfiles/wallpapers/current.jpg
-hyprctl hyprpaper wallpaper ', ~/.config/dotfiles/wallpapers/current.jpg,cover'
+awww img ~/.config/dotfiles/wallpapers/current.jpg --transition-type fade --transition-duration 3
 notify-send "Wallpaper changed: $1" -a "System" -i ~/.config/dotfiles/wallpapers/current.jpg
