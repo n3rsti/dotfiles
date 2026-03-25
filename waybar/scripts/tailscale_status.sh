c2c9352cd270a49fbea7aa035f91c29f91d66747
Krzysztof Witucki <n3rsti@protonmail.com>
2026-03-26 00:39:45 +0100

    bring back backgrounds for active states for some things

--- waybar/scripts/tailscale_status.sh
+++ waybar/scripts/tailscale_status.sh
@@ -2,7 +2,7 @@
 
 if tailscale status --json | jq -e '.ExitNodeStatus != null' >/dev/null; then
     data=$(tailscale exit-node list | grep 'selected' | awk '{print $2, $3}')
-    echo "{\"text\": \"   VPN\", \"tooltip\": \"$data\", \"class\": \"connected\"}"
+    echo "{\"text\": \"   Connected\", \"tooltip\": \"$data\", \"class\": \"connected\"}"
 else
-    echo '{"text": "    VPN", "tooltip": "Disconnected"}'
+    echo '{"text": "    Disconnected", "tooltip": "Disconnected"}'
 fi
